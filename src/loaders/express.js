96854a713e185ea65776f9da39f392fa5d83be68
Prathamesh More <pprathameshmore@outlook.com>
2020-07-20 12:53:37 +0530

    Add CROS URLs

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,13 +2,11 @@ module.exports = (app) => {
   const { config } = require("../configs/index");
   const express = require("express");
   const path = require("path");
-  const passport = require("passport");
   const cors = require("cors");
-  const session = require("express-session");
   const errorHandler = require("../api/middlewares/errorHandler");
   const apiRouter = require("../api/routes/index");
   require("./logger")(app);
-  app.use(
+  /*  app.use(
     session({
       secret: process.env.COOKIE_SECRET,
       resave: true,
@@ -17,13 +15,12 @@ module.exports = (app) => {
         sameSite: "none",
       },
     })
-  );
+  ); */
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, "public")));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  /* const whitelist = [
+
+  const whitelist = [
     "https://fshare.netlify.app",
     "http://localhost:3001",
     "http://localhost:3000",
@@ -33,11 +30,11 @@ module.exports = (app) => {
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error("You are not allowed to access this API"));
       }
     },
   };
-  app.use(cors(corsOptions)); */
+  app.use(cors(corsOptions));
   //API Routes
   app.use(`/${config.API_PREFIX}`, apiRouter);
   app.use(errorHandler);
