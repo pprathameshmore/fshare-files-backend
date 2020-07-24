f34664ba0860539bfabfc9770597fd8d394a0c56
Prathamesh More <pprathameshmore@outlook.com>
2020-07-24 15:13:18 +0530

    Update generate-sas-token.js

--- src/helpers/generate-sas-token.js
+++ src/helpers/generate-sas-token.js
@@ -13,8 +13,7 @@ exports.generateSASToken = () => {
   );
 
   const startDate = new Date();
-  startDate.setMinutes(startDate.getMinutes() - 1);
-  startDate.setSeconds(startDate.getSeconds() - 10);
+  startDate.setMinutes(startDate.getMinutes() - 5);
   const expireDate = new Date();
   expireDate.setMinutes(startDate.getMinutes() + 1);
 
