8094010cc5595aaa9656167254d2f327e91f7661
Prathamesh More <pprathameshmore@outlook.com>
2020-08-01 22:32:59 +0530

    Fix ziping problem

--- src/helpers/zip-files.js
+++ src/helpers/zip-files.js
@@ -1,15 +1,13 @@
 const AdmZip = require("adm-zip");
 
-const zip = new AdmZip();
-
 function compressFiles(files, archiverName, dirPath) {
+  const zip = new AdmZip();
   files.forEach((file) => {
     zip.addLocalFile(file.path, null, file.originalname);
   });
   zip.writeZip(dirPath + archiverName);
+  delete zip;
 }
-
-delete zip;
 
 module.exports = {
   compressFiles: compressFiles,
